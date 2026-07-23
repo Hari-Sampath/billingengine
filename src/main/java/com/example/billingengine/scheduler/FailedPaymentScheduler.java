f88246ad6cdcf459415e8349eb12e9374b22378c
Hari Sampath <harisampathbellam@gmail.com>
2026-07-23 14:44:38 +0530

    Create FailedPaymentScheduler.java

---
+++ src/main/java/com/example/billingengine/scheduler/FailedPaymentScheduler.java
@@ -0,0 +1,31 @@
+package com.example.billingengine.scheduler;
+
+import com.example.billingengine.entity.Transaction;
+import com.example.billingengine.entity.TransactionStatus;
+import com.example.billingengine.repository.TransactionRepository;
+import com.example.billingengine.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class FailedPaymentScheduler {
+
+    private final TransactionRepository transactionRepository;
+    private final EmailService emailService;
+
+    // Runs every day at 9:00 AM server time
+    @Scheduled(cron = "*/30 * * * * *")
+    public void checkFailedPayments() {
+        List<Transaction> failedUnnotified =
+                transactionRepository.findByStatusAndFailureEmailSentFalse(TransactionStatus.FAILED);
+
+        for (Transaction tx : failedUnnotified) {
+            emailService.sendFailureAlert(tx);
+            tx.setFailureEmailSent(true);
+            transactionRepository.save(tx);
+        }
+    }
+}
